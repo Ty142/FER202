ac98a67dffb14f62eaf2b6f6a0141b331a0871e9
Ty142 <tranduykhang159@gmail.com>
2025-01-09 11:41:22 +0700

    ex5

---
+++ FER202-ex/homework-ex5/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Course Name </h1>
+          <ul>
+            <li>  React </li>
+            <li>  ReactNative  </li>
+            <li>  NodeJs </li>
+          </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
