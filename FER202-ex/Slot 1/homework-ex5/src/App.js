c8ecb25530a890d1119f55b2574075fd552a09bb
Ty142 <tranduykhang159@gmail.com>
2025-01-09 13:00:53 +0700

    slot 1

--- FER202-ex/Slot 1/homework-ex5/src/App.js
+++ FER202-ex/Slot 1/homework-ex5/src/App.js
@@ -1,16 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Course Name </h1>
-          <ul>
-            <li>  React </li>
-            <li>  ReactNative</li>
-            <li>  NodeJs </li>
-          </ul>
+        <ul>
+          <li> React </li>
+          <li> ReactNative</li>
+          <li> NodeJs </li>
+        </ul>
       </header>
     </div>
   );
